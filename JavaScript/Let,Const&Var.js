7d0f5981fd60c771af910cf73da7811b90dd799e
Sri Achyut Vishnubhotla <112319695+Achyut-Vishnubhotla@users.noreply.github.com>
2024-03-13 22:09:36 +0530

    Add files via upload

---
+++ JavaScript/Let,Const&Var.js
@@ -0,0 +1,48 @@
+//uptill now variable declarations is done like 
+//a=5
+//but in real world this type of practice is not efficient
+
+
+//let keyword -> syntax of declaring variables
+
+let age = 23;
+
+//and the updation can be as usual
+age = age + 1;
+
+
+let cgpa;
+cgpa=8.9;
+
+
+let num1=1;
+let num2=2;
+let sum=num1+num2;
+
+//in let, the updation can be done
+
+ 
+
+//const 
+const newNum = 5;
+
+//but updation gives u error
+//newNum = 10;
+
+// const is used -> values of constants can't be chnages with re-assignment & they cant be redeclared
+
+const year = 2025;
+//year = 2026;   ->error
+//year = year + 1  ->error
+
+const pi=3.14;
+
+
+
+
+//var -> old syntax of writing variables
+//not used much
+var a = 23;
+var b = 8.9;
+
+var sum1 = a + b;
